f82297106f2df81edcac99cf430a0f29bbd4a6bb
deafyu <quuba68@wp.pl>
2022-01-16 14:28:07 +0100

    add security fix

--- src/main/java/com/example/loaner_back/entity/UserEntity.java
+++ src/main/java/com/example/loaner_back/entity/UserEntity.java
@@ -35,7 +35,7 @@ public class UserEntity {
     @OneToOne(mappedBy = "loanCreator")
     private LoanEntity user;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private Set<RoleEntity> roles;
 
     public void addRole(RoleEntity role){
